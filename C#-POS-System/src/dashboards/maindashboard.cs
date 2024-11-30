74cc4b7ca24849858f88e0c67715c90337ee836b
RajanithBandara <RajanithBandara123@gmail.com>
2024-11-30 16:06:07 +0530

    admin panel added

---
+++ C#-POS-System/src/dashboards/maindashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__POS_System.src.dashboards
+{
+    public partial class maindashboard : Form
+    {
+        public maindashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
