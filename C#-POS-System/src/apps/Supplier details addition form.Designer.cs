2cf6202327fd8522fa8ac7725f0a9c941f21acf7
Ransika01 <138203443+Ransika01@users.noreply.github.com>
2024-12-02 22:15:56 +0530

    addition

---
+++ C#-POS-System/src/apps/Supplier details addition form.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace C__POS_System.src.apps
+{
+    partial class Supplier_details_addition_form
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Supplier_details_addition_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Supplier_details_addition_form";
+            this.Size = new System.Drawing.Size(989, 588);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
