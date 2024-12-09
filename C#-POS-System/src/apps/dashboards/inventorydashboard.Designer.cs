31153cd242fe069ba04e7b2a9ad9b161199d2a13
RajanithBandara <RajanithBandara123@gmail.com>
2024-12-09 17:56:08 +0530

    ui enhancement

--- C#-POS-System/src/apps/dashboards/inventorydashboard.Designer.cs
+++ C#-POS-System/src/apps/dashboards/inventorydashboard.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.rjButton1 = new CustomControls.RJControls.RJButton();
+            this.rjPanel1 = new RoundedPanelClass.RJPanel();
+            this.rjPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(398, 12);
+            this.panel1.Location = new System.Drawing.Point(353, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1447, 975);
+            this.panel1.Size = new System.Drawing.Size(1492, 975);
             this.panel1.TabIndex = 1;
             // 
             // rjButton1
@@ -50,7 +52,7 @@
             this.rjButton1.FlatAppearance.BorderSize = 0;
             this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rjButton1.ForeColor = System.Drawing.Color.White;
-            this.rjButton1.Location = new System.Drawing.Point(48, 61);
+            this.rjButton1.Location = new System.Drawing.Point(57, 65);
             this.rjButton1.Name = "rjButton1";
             this.rjButton1.Size = new System.Drawing.Size(150, 40);
             this.rjButton1.TabIndex = 0;
@@ -58,22 +60,37 @@
             this.rjButton1.TextColor = System.Drawing.Color.White;
             this.rjButton1.UseVisualStyleBackColor = false;
             // 
+            // rjPanel1
+            // 
+            this.rjPanel1.BackColor = System.Drawing.Color.Gray;
+            this.rjPanel1.BorderRadius = 30;
+            this.rjPanel1.Controls.Add(this.rjButton1);
+            this.rjPanel1.ForeColor = System.Drawing.Color.White;
+            this.rjPanel1.GradientAngle = 90F;
+            this.rjPanel1.GradientBottomColor = System.Drawing.Color.Gray;
+            this.rjPanel1.GradientTopColor = System.Drawing.Color.Gray;
+            this.rjPanel1.Location = new System.Drawing.Point(12, 12);
+            this.rjPanel1.Name = "rjPanel1";
+            this.rjPanel1.Size = new System.Drawing.Size(335, 975);
+            this.rjPanel1.TabIndex = 0;
+            // 
             // inventorydashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1857, 999);
+            this.Controls.Add(this.rjPanel1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.rjButton1);
             this.Name = "inventorydashboard";
             this.Text = "inventorydashboard";
+            this.rjPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private CustomControls.RJControls.RJButton rjButton1;
         private System.Windows.Forms.Panel panel1;
+        private CustomControls.RJControls.RJButton rjButton1;
+        private RoundedPanelClass.RJPanel rjPanel1;
     }
 }
