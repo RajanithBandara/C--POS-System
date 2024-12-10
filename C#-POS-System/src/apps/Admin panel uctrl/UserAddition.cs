ca890d9f6573d83edc74cfa8beb5c6b6c647dbbd
wethmi <135610348+wethmi@users.noreply.github.com>
2024-12-10 23:55:49 +0530

    User addtion User control

---
+++ C#-POS-System/src/apps/Admin panel uctrl/UserAddition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_POS_System.src.apps.Admin_panel_uctrl
+{
+    public partial class UserAddition : UserControl
+    {
+        public UserAddition()
+        {
+            InitializeComponent();
+        }
+
+        private void rjButton2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+        }
+    }
+}
