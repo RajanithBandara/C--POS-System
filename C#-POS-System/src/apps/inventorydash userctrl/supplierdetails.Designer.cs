622786bd387041e654ae50a515454d7e73a2fe20
RajanithBandara <RajanithBandara123@gmail.com>
2024-12-10 14:48:40 +0530

    main dashboard added

---
+++ C#-POS-System/src/apps/inventorydash userctrl/supplierdetails.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CSharp_POS_System.src.apps.inventorydash_userctrl
+{
+    partial class supplierdetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rjPanel1 = new RoundedPanelClass.RJPanel();
+            this.SuspendLayout();
+            // 
+            // rjPanel1
+            // 
+            this.rjPanel1.BackColor = System.Drawing.Color.Gray;
+            this.rjPanel1.BorderRadius = 30;
+            this.rjPanel1.ForeColor = System.Drawing.Color.White;
+            this.rjPanel1.GradientAngle = 90F;
+            this.rjPanel1.GradientBottomColor = System.Drawing.Color.Gray;
+            this.rjPanel1.GradientTopColor = System.Drawing.Color.Gray;
+            this.rjPanel1.Location = new System.Drawing.Point(47, 68);
+            this.rjPanel1.Name = "rjPanel1";
+            this.rjPanel1.Size = new System.Drawing.Size(1398, 848);
+            this.rjPanel1.TabIndex = 0;
+            // 
+            // supplierdetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.rjPanel1);
+            this.Name = "supplierdetails";
+            this.Size = new System.Drawing.Size(1492, 975);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RoundedPanelClass.RJPanel rjPanel1;
+    }
+}
