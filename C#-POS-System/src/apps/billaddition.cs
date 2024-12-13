d20ce9502be2a55a65b4cd69818ee3dc80fed76f
wethmi <135610348+wethmi@users.noreply.github.com>
2024-12-13 23:47:48 +0530

    bill addition usercontrol

---
+++ C#-POS-System/src/apps/billaddition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_POS_System.src.apps
+{
+    public partial class billaddition : UserControl
+    {
+        public billaddition()
+        {
+            InitializeComponent();
+        }
+
+        private void rjButton2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+        }
+    }
+}
