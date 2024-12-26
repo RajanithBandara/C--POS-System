badfa6313d4c86186903fe9f001cfdb46502dfac
RajanithBandara <RajanithBandara123@gmail.com>
2024-12-26 10:56:51 +0530

    changes

--- C#-POS-System/Program.cs
+++ C#-POS-System/Program.cs
@@ -21,7 +21,7 @@ namespace C__POS_System
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Admin_panel());
+            Application.Run(new inventorydashboard());
 
 
         }
